38e1d34e4563c357e72a691ec181fd31ef724398
Illia <kargozaurus@gmail.com>
2026-09-04 00:06:58 +0400

    gracefull shutdown of the grpc server

--- svc/order/main.go
+++ svc/order/main.go
@@ -6,8 +6,13 @@ import (
 	"net"
 	processor "order/event_processor"
 	"order/server"
+	"os"
+	"os/signal"
 	"pkg/envreader"
 	orderv1 "proto/out/order/v1"
+	"sync"
+	"syscall"
+	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	"google.golang.org/grpc"
@@ -30,7 +35,8 @@ func initServer() *grpc.Server {
 }
 
 func main() {
-	ctx := context.Background()
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
 	pool, err := initDB(ctx)
 	if err != nil {
 		log.Fatal(err.Error())
@@ -43,8 +49,30 @@ func main() {
 	defer listener.Close()
 	grpcServer := initServer()
 	proc := processor.NewProcessor()
-	go proc.Run(ctx)
-	if err := grpcServer.Serve(listener); err != nil {
-		log.Fatal(err.Error())
+	defer proc.Close()
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		proc.Run(ctx)
+	}()
+	go func() {
+		defer wg.Done()
+		if err := grpcServer.Serve(listener); err != nil {
+			log.Println(err.Error())
+		}
+	}()
+	<-ctx.Done()
+	stopped := make(chan struct{})
+	go func() {
+		grpcServer.GracefulStop()
+		close(stopped)
+	}()
+	select {
+	case <-stopped:
+	case <-time.After(time.Second * 10):
+		log.Println("enforce stop")
+		grpcServer.Stop()
 	}
+	wg.Wait()
 }
